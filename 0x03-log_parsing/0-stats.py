d3eab7843997ffbc02d7308c2eed495dda7128d6
dainahwanjiku <deeshish2017@gmail.com>
2023-01-21 11:07:06 +0000

    stats

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a script that reads stdin line by line and computes metrics
+"""
+
+import sys
+import fileinput
+
+"""  """
+
+import fileinput
+
+for line in fileinput.input():
+    pass
+
