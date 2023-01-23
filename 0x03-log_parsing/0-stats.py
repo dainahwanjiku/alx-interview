e27fe91d8dcd6bdc5d93f4c6025191bd695ee4c9
dainahwanjiku <deeshish2017@gmail.com>
2023-01-23 10:58:54 +0000

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
-""" reads stdin line by line and computes metrics """
+"""reads stdin line by line and computes metrics"""
 
 import sys
 
 
 def print_status(dic, size):
-    """ Prints information """
+    """Prints information"""
     print("File size: {:d}".format(size))
     for i in sorted(dic.keys()):
         if dic[i] != 0:
             print("{}: {:d}".format(i, dic[i]))
 
 
-# sourcery skip: use-contextlib-suppress
-statusCodes = { "200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-               "404": 0, "405": 0, "500": 0 }
+#sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
 
 count = 0
 size = 0
