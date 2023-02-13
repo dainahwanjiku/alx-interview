762309e02e2883a03df788ed2552aa006810804c
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2023-02-13 04:41:33 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,53 @@
+#!/usr/bin/node
 
+const request = require('request');
+
+const movieId = process.argv[2];
+const filmUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+let people = [];
+const names = [];
+
+const reqCharacters = async () => {
+  await new Promise(resolve => request(filmUrl, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
+    }
+  }));
+};
+
+const reqNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
+  } else {
+    console.error('Error: Did not get any Characters');
+  }
+};
+
+const getCharNames = async () => {
+  await reqCharacters();
+  await reqNames();
+
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
+    }
+  }
+};
+
+getCharNames();
